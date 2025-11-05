d07a327e052bc1ef47f1f85023d4480b1c2f1d0a
rnozila2197 <yjamshid2197@gmail.com>
2025-11-05 18:12:24 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,94 @@
+# # 1
+# for i in range(1, 51):
+#     print(i)
+#     if i == 42:
+#         break
+#
+# # 2
+# for i in range(1, 21):
+#     if i % 2 == 0:
+#         print(i)
+#
+# # 3
+# for i in range(1, 31):
+#     print(i)
+#     if i % 5 == 0:
+#         break
+#
+# # 4
+# for i in range(1, 16):
+#     if i % 3 != 0:
+#         print(i)
+#
+# # 5
+# for i in range(1, 21):
+#     if i == 25:
+#         print("bor")
+#
+# # 6
+# for i in range(1, 51):
+#     print(i)
+#     if i % 7 == 0:
+#         break
+#
+# # 7
+# for i in range(1, 21):
+#     print(i)
+#     if i == 10:
+#         break
+
+# # 8
+# for i in range(1, 16):
+#     if i % 3 == 0:
+#         print(i)
+
+# # 9
+# for i in range(10, 1, -1):
+#     print(i)
+#     if i == 5:
+#         break
+
+# # 10
+# for i in range(1, 26):
+#     if i % 4 == 0:
+#         print(i)
+
+# # 11
+# for i in range(1, 21):
+#     print(i)
+#     if i == 15:
+#         break
+
+# # 12
+# for i in range(1, 101):
+#     print(i)
+#     if i % 9 == 0:
+#         break
+
+# # 13
+# for i in range(1, 21):
+#     if i % 2 or 3 != 0:
+#         print(i)
+
+# # 14
+# for i in range(1, 21):
+#     if i % 2 == 0:
+#         print(i)
+
+# # 15
+# for i in range(1, 8):
+#     if i % 8 == 0:
+#         print("8 topilmadi!")
+#         break
+
+
+# # 16
+# for i in range(15, 1, -1):
+#     if i % 3 == 0:
+#         print(i)
+
+# # 17
+# for i in range(1, 21):
+#     print(i)
+#     if i == 12:
+#         break
